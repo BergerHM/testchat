f9766576c0f73bd60600c2c47a71eb5fca46556e
Lukas Altenstrasser <altenstrasser.lukas@gmail.com>
2020-06-14 22:05:28 +0200

    methode für text suche init commit
    und htmlparser als eigentständiges pythonfile hinzugefügt

--- connector/confluence_calls.py
+++ connector/confluence_calls.py
@@ -1,6 +1,8 @@
 # Methods for searching Confluence
-from html.parser import HTMLParser
+
 import requests
+
+from connector.htmltableparser import HTMLTableParser
 
 
 class ConfluenceSearch:
@@ -8,6 +10,9 @@ class ConfluenceSearch:
     headers = {
         'Authorization': 'Basic bHVrYXMuYWx0ZW5zdHJhc3NlckBobS5lZHU6YjBWbGpSc3pxRUFQWE1qQnlmMEdCMEQ4'
     }
+
+    def __init__(self):
+        self.parser = HTMLTableParser()
 
     def get_sites(self):
         url = "https://ccwi.atlassian.net/wiki/rest/api/content"
@@ -36,14 +41,9 @@ class ConfluenceSearch:
         response = requests.request("GET", url, headers=headers, data=payload)
         json_response = response.json()
         html = json_response['body']['storage']['value']
-        parser = HTMLTableParser()
-        parser.feed(html)
-        # print(len(parser.tables[0]))
-        # for i in range(1,8):
-        #     if (parser.tables[0][i][0] == role):
-        #         for j in range(0, 3):
-        #             array.append(parser.tables[0][i][j])
-        for i in parser.tables[0]:
+        self.parser.reset()
+        self.parser.feed(html)
+        for i in self.parser.tables[0]:
             if i[0] == role:
                 data.append(i)
         return data
@@ -59,77 +59,27 @@ class ConfluenceSearch:
         response = requests.request("GET", url, headers=headers, data=payload)
         json_response = response.json()
         html = json_response['body']['storage']['value']
-        parser = HTMLTableParser()
-        parser.feed(html)
-        for i in parser.tables[0]:
+        self.parser.reset()
+        self.parser.feed(html)
+        for i in self.parser.tables[0]:
             if i[0] != "Rolle":
                 array.append(i[0])
             # Remove duplicates
             array = list(dict.fromkeys(array))
         return array
 
+    def text_search(self, search):
+        results = []
+        url = "https://ccwi.atlassian.net/wiki/rest/api/content/search?cql=text~Coach"
+        payload = {}
+        headers = {
+            'Authorization': 'Basic bHVrYXMuYWx0ZW5zdHJhc3NlckBobS5lZHU6YjBWbGpSc3pxRUFQWE1qQnlmMEdCMEQ4'
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        json_response = response.json()
+        for i in json_response['results']:
+            results.append(i)
+        return results
 
-class HTMLTableParser(HTMLParser):
-    """ This class serves as a html table parser. It is able to parse multiple
-    tables which you feed in. You can access the result per .tables field.
-    """
-
-    def __init__(
-            self,
-            decode_html_entities=False,
-            data_separator=' ',
-    ):
-
-        HTMLParser.__init__(self)
-
-        self._parse_html_entities = decode_html_entities
-        self._data_separator = data_separator
-
-        self._in_td = False
-        self._in_th = False
-        self._current_table = []
-        self._current_row = []
-        self._current_cell = []
-        self.tables = []
-
-    def handle_starttag(self, tag, attrs):
-        """ We need to remember the opening point for the content of interest.
-        The other tags (<table>, <tr>) are only handled at the closing point.
-        """
-        if tag == 'td':
-            self._in_td = True
-        if tag == 'th':
-            self._in_th = True
-
-    def handle_data(self, data):
-        """ This is where we save content to a cell """
-        if self._in_td or self._in_th:
-            self._current_cell.append(data.strip())
-
-    def handle_charref(self, name):
-        """ Handle HTML encoded characters """
-
-        if self._parse_html_entities:
-            self.handle_data(self.unescape('&#{};'.format(name)))
-
-    def handle_endtag(self, tag):
-        """ Here we exit the tags. If the closing tag is </tr>, we know that we
-        can save our currently parsed cells to the current table as a row and
-        prepare for a new row. If the closing tag is </table>, we save the
-        current table and prepare for a new one.
-        """
-        if tag == 'td':
-            self._in_td = False
-        elif tag == 'th':
-            self._in_th = False
-
-        if tag in ['td', 'th']:
-            final_cell = self._data_separator.join(self._current_cell).strip()
-            self._current_row.append(final_cell)
-            self._current_cell = []
-        elif tag == 'tr':
-            self._current_table.append(self._current_row)
-            self._current_row = []
-        elif tag == 'table':
-            self.tables.append(self._current_table)
-            self._current_table = []
+search = ConfluenceSearch()
+search.text_search("Coach")
