1ba97a96eb22026a7a95751c59645a53fa5616a1
Mia <Sqfsbo5$>
2020-07-07 16:59:45 +0200

    Aktualisierung Luis-App Credentials

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = "abc596b2-7f1a-49d2-89fd-8ab41ef8d068"
-    APP_PASSWORD = "_cj1G-E0AxkCJVV?OMpUGdyYnKn-nR47"
+    APP_ID = "d7e37f51-7f27-47ef-802b-5519f76f383f"
+    APP_PASSWORD = "AtLeastSixteenCharacters_0"
     LUIS_APP_ID = os.environ.get("36901912-db40-4303-8dac-58b3bcc43390", "")
     LUIS_API_KEY = os.environ.get("23d11aa114794c4fa1fcab35a09b9e97", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
